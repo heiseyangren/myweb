4fdc1a775838864d39639f9af9bb8dbca427ad74
iver66 <wyygyxllz@163.com>
2017-03-01 16:04:33 +0800

    Initial commit

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,25 @@
 <html>
 <head>
 <title>@yield('title','Myweb')-Learn Laravel</title>
+<link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+<header class="navbar navbar-fixed-top navbar-inverse">
+<div class="container">
+<div class="col-md-offset-1 col-md-10">
+<a href="/" id="logo">Myweb</a>
+<nav>
+<ul class="nav navbar-nav navbar-right">
+<li><a href="/help">help</a></li>
+<li><a href="#">login</a></li>
+</ul>
+</nav>
+</div>
+</div>
+</header>
+
+<div class="container">
 @yield('content')
+</div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -6,7 +6,18 @@
 <body>
 @extends('layouts.default')
 @section('content')
-<h1>Home Page</h1> <!--zhuye-->
+<div class="jumbotron">
+<h1>hello laravel</h1>
+<p class="lead">
+this is the home page of learn laravel
+</p>
+<p>
+everything start from here
+</p>
+<p>
+<a class="btn btn-lg btn-success" href="#" role="button">sign up now</a>
+</p>
+</div> 
 @stop
 </body>
 </html>
